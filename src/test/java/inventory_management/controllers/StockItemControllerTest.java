08a4b1f2cb31fe26f3a66fe58eba3b06b9c61985
ESPIE <remi.espie@etu.umontpellier.fr>
2024-04-29 11:35:28 +0200

    Adds StockItem service implementation

--- src/test/java/inventory_management/controllers/StockItemControllerTest.java
+++ src/test/java/inventory_management/controllers/StockItemControllerTest.java
@@ -1,6 +1,15 @@
-package inventory_management.controllers;
+package com.ibm.inventory_management.controllers;
 
-import com.ibm.inventory_management.controllers.StockItemController;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -8,20 +17,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Mockito.spy;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.ibm.inventory_management.models.StockItem;
+import com.ibm.inventory_management.services.StockItemApi;
 
 @DisplayName("StockItemController")
 public class StockItemControllerTest {
     StockItemController controller;
+    StockItemApi service;
 
     MockMvc mockMvc;
 
     @BeforeEach
     public void setup() {
-        controller = spy(new StockItemController());
+        service = mock(StockItemApi.class);
+
+        controller = spy(new StockItemController(service));
 
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
@@ -44,6 +54,17 @@ public class StockItemControllerTest {
 
             mockMvc.perform(get("/stock-items").accept("application/json"))
                     .andExpect(content().json("[]"));
+        }
+
+        @Test
+        @DisplayName("When called then it should return the results of the StockItemService")
+        public void when_called_then_return_the_results_of_the_stockitemservice() throws Exception {
+
+            final List<StockItem> expectedResult = Arrays.asList(new StockItem());
+            when(service.listStockItems()).thenReturn(expectedResult);
+
+            mockMvc.perform(get("/stock-items").accept("application/json"))
+                    .andExpect(content().json("[{}]"));
         }
     }
 }
